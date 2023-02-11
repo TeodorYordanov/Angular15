2cba8d88bec1f3170ec4e1706b0b216278898b89
teodoryanchev <teodoryordanov1992@gmail.com>
2023-02-11 13:33:11 +0100

    Add Demo JavaScriptFile

---
+++ JavaSciptFile/src/app/component/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
+
+declare function changeColor():void;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+
+export class HomeComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      changeColor();
+    }, 2000);
+  }  
+
+}
